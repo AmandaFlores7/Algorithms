7ad29304d94911e056b918d807d17dadab8ce349
Amanda Flores <amanda.flores.a@mail.pucv.cl>
2021-11-16 21:37:54 -0300

    sincronizacion

--- Algorithms/src/main/java/Hilos/SincronizacionDeHilos/ClasePrincipal.java
+++ Algorithms/src/main/java/Hilos/SincronizacionDeHilos/ClasePrincipal.java
@@ -10,5 +10,35 @@ package main.java.Hilos.SincronizacionDeHilos;
  * @author amand
  */
 public class ClasePrincipal {
-    
+    public static void main(String[] args) {
+        
+        Hilo1 hilo1 = new Hilo1();
+        Hilo2 hilo2 = new Hilo2();
+        Hilo3 hilo3 = new Hilo3();
+        Hilo4 hilo4 = new Hilo4();
+        
+        hilo1.start();
+        try {
+            hilo1.sleep(10);
+        } catch (Exception e) {
+        }
+        
+        hilo2.start();
+        try {
+            hilo2.sleep(10);
+        } catch (Exception e) {
+        }
+        
+        hilo3.start();
+        try {
+            hilo3.sleep(10);
+        } catch (Exception e) {
+        }
+        
+        hilo4.start();
+        try {
+            hilo4.sleep(10);
+        } catch (Exception e) {
+        }
+    }
 }
